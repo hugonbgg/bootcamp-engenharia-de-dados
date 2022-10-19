5c31be0e013ffd403c3243ce84993576ef4a8016
Hugo Nicolau Barbosa de Gusmão <hugonbgg@gmail.com>
2022-10-19 17:53:25 -0300

    cria função e add a cotação no momento, a min e a max

--- Fundamentos de Ingestão de Dados/cotacao.py
+++ Fundamentos de Ingestão de Dados/cotacao.py
@@ -32,5 +32,24 @@ dolar# %%
 #printa a data de hoje e o valor da cotação
 print(f"20 doláres hoje ({dolar['create_date'][:10]}) custam {round(float(dolar['bid']) * 20, 2)} reais")
 
-# %%
+#%%
 #Salvando o codigo em uma função
+def cotacao(qtd, moeda):
+    url = f'https://economia.awesomeapi.com.br/json/last/{moeda}'
+    
+    ret = requests.get(url)
+    moeda_cotacao = json.loads(ret.text)[moeda.replace('-','')]
+    print(f'''
+        {qtd} {moeda[:3]} custam hoje {round(float(moeda_cotacao['bid']) * 20)} {moeda[4:]}. 
+        A cotação máxima do dia foi {float(moeda_cotacao['high']).__round__(2)} {moeda[4:]} e a minima {float(moeda_cotacao['low']).__round__(2)} {moeda[4:]}.
+        ''')
+    print(moeda_cotacao)
+ 
+#%%
+cotacao(20,'USD-BRL')
+
+
+# %%
+
+
+# %%
