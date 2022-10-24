e21723cf676a30b581a6fff6e07e54caac8fe76b
Hugo Nicolau Barbosa de Gusmão <hugonbgg@gmail.com>
2022-10-24 15:27:20 -0300

    fix logs.py, registrador de log antes da função

--- Fundamentos de Ingestão de Dados/logs.py
+++ Fundamentos de Ingestão de Dados/logs.py
@@ -4,16 +4,26 @@ import logging
 import backoff
 
 #%%
+#salvando o logs utilizando o logging
+log = logging.getLogger()
+log.setLevel(logging.DEBUG) #define o nivel do log
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+channel = logging.StreamHandler()
+channel.setFormatter(formatter)
+log.addHandler(channel)
+
+
+# %%
 #Simulando a API e usando o backoff
 @backoff.on_exception(backoff.expo, (ConnectionAbortedError, ConnectionRefusedError, TimeoutError), max_tries=10)
 def test_func(*args, **kargs):
-    
+   
     rnd = random.randint(100, 1000)
     log.debug(f"RND: {rnd}")
     log.info(f"args: {args if args else 'sem args'}")
     log.info(f"kargs: {kargs if kargs else 'sem kargs'}")
-    
-    
+   
     if rnd < 200:
         log.error('Conexão foi finalizada')
         raise ConnectionAbortedError('Conexão foi finalizada')
@@ -27,19 +37,7 @@ def test_func(*args, **kargs):
     
     else:
         return "OK!"
-    
-#%%
-test_func()# %%
+
 # %%
-
-#salvando o logs utilizando o logging
-log = logging.getLogger()
-log.setLevel(logging.DEBUG) #define o nivel do log
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-channel = logging.StreamHandler()
-channel.setFormatter(formatter)
-log.addHandler(channel)
-
-
+test_func()
 # %%
