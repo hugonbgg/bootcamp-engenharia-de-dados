dfd077a5e5fbb100ec514302cb0175feab660ad1
Hugo Nicolau Barbosa de Gusmão <hugonbgg@gmail.com>
2022-10-25 12:33:00 -0300

    imoveis parte 1 finalizada. Obtendo as infos da primeira pagina

--- Fundamentos de Ingestão de Dados/imoveis.py
+++ Fundamentos de Ingestão de Dados/imoveis.py
@@ -1,3 +1,32 @@
+#%%
 import requests
-import pandas
-import bs4
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+
+# %%
+url = 'https://www.vivareal.com.br/venda/parana/curitiba/apartamento_residencial/?pagina={}'
+ret = requests.get(url)
+ret.text
+# %%
+soup = bs(ret.text, 'html.parser')
+# %%
+soup.prettify()
+# %%
+soup.find('a', attrs={'class':'property-card__content-link js-card-title'})
+soup.find('a', attrs={'class':'property-card__content-link js-card-title'}).text
+
+# %%
+soup.find_all('a', attrs={'class':'property-card__content-link js-card-title'})
+# %%
+imoveis = soup.find_all('a', attrs={'class':'property-card__content-link js-card-title'})
+imoveis
+# %%
+for imovel in imoveis:
+    print (imovel.text)
+# %%
+len(imoveis)
+# %%
+qtd_imoveis = int(soup.find('strong', attrs= {'class':'results-summary__count js-total-records'}).text.replace('.',''))
+qtd_imoveis# %%
+
+# %%
