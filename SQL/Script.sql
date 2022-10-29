296f9d32be1cc12f044f7e464500c84b64b43765
Hugo Nicolau Barbosa de Gusmão <hugonbgg@gmail.com>
2022-10-29 16:32:52 -0300

    CTE e Window Function

--- SQL/Script.sql
+++ SQL/Script.sql
@@ -10,46 +10,9 @@ artist varchar(300) null,
 "weeks-on-board" int4 null
 );
 
-select * from "Billboard" limit 100;
-select count (*) as quantidade from "Billboard";
-select artist, song, "weeks-on-board"  from "Billboard" b where "weeks-on-board" > 50;
+select * from "Billboard" b 
 
-
-SELECT t1."date"
-	,t1."rank"
-	,t1.song
-	,t1.artist
-	,t1."last-week"
-	,t1."peak-rank"
-	,t1."weeks-on-board"
-FROM "Billboard" AS t1
-
-	
-
-SELECT t1.song
-	,t1.artist
-FROM "Billboard" AS t1
-where t1.artist = 'Chuck Berry'
-
-
-select distinct t1.artist
-	, t1.song
-	, count(*) as "#song"
-from "Billboard" as t1
-where t1.artist = 'Chuck Berry'
-group by t1.artist, t1.song 
-order by "#song" desc
-	
-select distinct t1.artist
-	, t1.song
-	, count(*) as "#song"
-from "Billboard" as t1
--- where t1.artist = 'Chuck Berry' or t1.artist = 'Frankie Vaughan'
-where t1.artist in ('Chuck Berry', 'Frankie Vaughan')
-group by t1.artist, t1.song 
-order by "#song" desc
-
---CTE
+-CTE
 --Tabelas distintas
 select distinct t1.artist
 	,t1.song
@@ -112,3 +75,76 @@ from "Billboard" as t1
 left join cte_artist as t2 on (t1.artist = t2.artist)
 left join cte_song as t3 on (t1.song = t3.song)
 order by t1.artist, t1.song
+
+----Window function 
+
+with cte_billboard 
+as (
+	select distinct 
+	t1.artist,
+	t1.song
+from "Billboard" as t1
+order by t1.artist, t1.song
+)
+select *,
+row_number () over(order by artist, song) as "row_number",
+row_number () over(partition by artist order by artist, song) as "row_number_by_artist"
+from cte_billboard;
+
+----
+with cte_billboard 
+as (
+select distinct 
+	t1.artist,
+	t1.song,
+	row_number () over(
+	order by artist,
+	song) 
+	as "row_number",
+	row_number () over(partition by artist
+order by
+	artist,	song) 
+	as "row_number_by_artist"
+from
+	"Billboard" as t1
+order by
+	t1.artist, t1.song
+)
+select
+	*
+from
+	cte_billboard
+	where "row_number_by_artist" = 1
+--------------
+	
+WITH cte_billboard
+AS (
+	SELECT DISTINCT t1.artist,
+		t1.song
+		FROM "Billboard" AS t1
+	ORDER BY t1.artist,
+		t1.song
+	)
+select *, 
+--row_number() OVER (ORDER BY artist,song) AS "row",
+--row_number() OVER (PARTITION BY artist ORDER BY artist,	song) AS "row_by_artist",
+rank() over(partition by artist order by artist, song) as "rank",
+--lag(song, 1) over(partition by artist order by artist, song) as "lag_song",
+--lead(song, 1) over(partition by artist order by artist, song) as "lead_song",
+first_value (song) over(partition by artist order by artist, song) as "first_song",
+last_value (song) over(partition by artist order by artist, song range between unbounded preceding and unbounded following) as "last_song"
+FROM cte_billboard
+
+
+
+
+
+
+
+
+
+
+
+
+
+
