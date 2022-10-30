45e11614bab428dd9441897eb47b43da98e81bc5
Hugo Nicolau Barbosa de Gusmão <hugonbgg@gmail.com>
2022-10-29 21:41:36 -0300

    criação de views

--- SQL/Script.sql
+++ SQL/Script.sql
@@ -135,16 +135,177 @@ first_value (song) over(partition by artist order by artist, song) as "first_son
 last_value (song) over(partition by artist order by artist, song range between unbounded preceding and unbounded following) as "last_song"
 FROM cte_billboard
 
+------------------------
+---primeira vez na billboard
+with cte_dedupli_artist 
+as (
+select t1.artist, t1."rank", t1."date",
+row_number () over (partition by artist order by artist, date) as "artist_row"
+from "Billboard" as t1
+order by t1.artist, t1."date")
+SELECT * FROM cte_dedupli_artist
+WHERE artist_row = 1
 
+---primeira vez na billboard e com qual musica
+with cte_dedup_artist 
+as (
+select t1.artist, t1.song, t1."rank", t1."date",
+row_number () over (partition by artist, song order by artist, date) as "artist_row"
+from "Billboard" as t1
+order by t1.artist, t1."date")
+select * from cte_dedup_artist
+where artist_row = 1
 
+---Criando tabela com o resultado da query
+create table website as (
+	with cte_dedupli_artist 
+	as (
+	select t1.artist, 
+	t1.song, 
+	t1."rank", 
+	t1."date",
+	row_number () over (partition by artist, song order by artist, date) as "artist_row"
+	from "Billboard" as t1
+	order by t1.artist, t1."date")
+	select artist, song, "rank", "date" from cte_dedupli_artist
+	where artist_row = 1
+);
 
+drop table website;
+select * from website 
 
+---criando uma tabela intermediaria para os exemplos a frente. Selecionado apenas o AC/DC
+create table tb_artist as (
+	select t1.artist, 
+	t1.song,
+	t1."rank", 
+	t1."date"
+	from "Billboard" as t1
+	where artist = 'AC/DC'
+	order by t1.artist, t1."date")
+;
+	
+select * from tb_artist;
+drop table tb_artist
 
+---criando uma view a partir da tabela criada anteriormente,tb_artist. Substitiu a billboard pela tb_artist, o resto ficou tudo igual.
+---View dos artistas selecionados
 
+create view vw_artist as (
+	with cte_dedupli_artist as (
+		select t1.artist,
+			t1."rank",
+			t1."date",
+			row_number() over (
+				partition by artist
+				order by artist,
+					"date"
+				) as "artist_row"
+		from tb_artist as t1
+		order by t1.artist,
+			t1."date"
+		) select artist,
+	"rank",
+	"date"
+	from cte_dedupli_artist where artist_row = 1
+)
+	
+-- para dropar: drop view vw_artist; 
+drop view vw_artist;
+select * from vw_artist;
 
+------Adicionando U2 a tabela tb_artist usando o insert
+insert into tb_artist (
+	select t1.artist, 
+	t1.song,
+	t1."rank", 
+	t1."date"
+	from "Billboard" as t1
+	where artist = 'U2'
+	order by t1.artist, t1."date")
+	
+------Adicionando os Elvis a tabela tb_artist usando o insert
+insert into tb_artist (
+	select t1.artist, 
+	t1.song,
+	t1."rank", 
+	t1."date"
+	from "Billboard" as t1
+	where artist like 'Elvis%'
+	order by t1.artist, t1."date")
+	
+	select * from tb_artist ta 
+	
+-----Verificando a view da tabela tb_artist
+select * from vw_artist va 
 
+---criando a tabela songs
 
+--- criando uma view dos songs
+create view vw_song as (
+	with cte_dedupli_artist as (
+		select t1.artist,
+			t1.song,
+			t1."rank",
+			t1."date",
+			row_number() over (
+				partition by artist, song
+				order by artist,
+				song,
+				"date"					
+				) as "artist_row"
+		from tb_artist as t1
+		order by t1.artist,t1."date", 
+		t1.song			
+		) select artist,
+				song,
+				"rank",
+				"date"
+	from cte_dedupli_artist where artist_row = 1
+)
 
+drop view vw_song;
+select * from vw_song vs 
 
+---Fazendo novo insert na tabela base tb_artist
+insert into tb_artist (
+	select t1.artist, 
+	t1.song,
+	t1."rank", 
+	t1."date"
+	from "Billboard" as t1
+	where artist like 'Adele%'
+	order by t1.artist, t1."date")
+	
+	select * from tb_artist
 
+select * from vw_song;
+select * from vw_artist va 
 
+---Alterando a view, retirando os artistas
+drop view vw_song;
+
+create view vw_song as (
+	with cte_dedupli_artist as (
+		select 
+			t1.song,
+			t1."rank",
+			t1."date",
+			row_number() over (
+				partition by song
+				order by 
+				song,
+				"date"					
+				) as "artist_row"
+		from tb_artist as t1
+		order by t1.song,	
+		t1."date"
+		) select 
+				song,
+				"rank",
+				"date"
+	from cte_dedupli_artist 
+	where artist_row = 1
+)
+
+select * from vw_song; 
