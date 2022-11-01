445e947aef657986ecce16ec863095a15fae46ad
Hugo Nicolau Barbosa de Gusmão <hugonbgg@gmail.com>
2022-11-01 17:19:56 -0300

    create pessoa class.

--- 04 - Capturando dados de uma API/orientacao_a_objetos.py
+++ 04 - Capturando dados de uma API/orientacao_a_objetos.py
@@ -1,11 +1,21 @@
+# import requisites
 import datetime
+import math
 
 
+# create class
 class Pessoa:
     def __init__(self, nome: str, sobrenome: str, data_de_nascimento: datetime.date):
         self.nome = nome
         self.sobrenome = sobrenome
         self.data_de_nascimento = data_de_nascimento
+
+    @property
+    def idade(self) -> int:
+        return math.floor((datetime.date.today() - self.data_de_nascimento).days / 365.2425)
+
+    def __str__(self) -> str:
+        return f"{self.nome} {self.sobrenome} {self.idade} anos."
 
 
 hugo = Pessoa(nome='Hugo', sobrenome='Gusmão', data_de_nascimento=datetime.date(1986, 2, 17))
@@ -14,3 +24,5 @@ print(hugo)
 print(hugo.nome)
 print(hugo.sobrenome)
 print(hugo.data_de_nascimento)
+
+print(hugo.idade)
