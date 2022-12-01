e72c2b0d031364c5a0b3874e98a9ff2ddc3dbb05
Hugo Nicolau Barbosa de Gusmão <hugonbgg@gmail.com>
2022-12-01 19:02:10 -0300

    definindo endpoints e types

--- 04 - Capturando dados de uma API/ingestao.py
+++ 04 - Capturando dados de uma API/ingestao.py
@@ -1,19 +1,62 @@
+from abc import ABC, abstractmethod
 import requests
+import logging
+import datetime
+
+# o __name__ se refere ao nome do arquivo, o nome do logger será esse.
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 
-class MercadoBitcoinApi():
+class MercadoBitcoinApi(ABC):
 
     def __init__(self, coin):
         self.coin = coin
         self.base_endpoint = 'https://www.mercadobitcoin.net/api'
 
-    def _get_endpoint(self):
-        return f"{self.base_endpoint}/{self.coin}/day-summary/2022/11/29"
+    @abstractmethod
+    def _get_endpoint(self, **kwargs):
+        pass
 
-    def get_data(self):
-        endpoint = self._get_endpoint()
+    def get_data(self, **kwargs):
+        endpoint = self._get_endpoint(**kwargs)
+        logger.info(f'Getting data from endpoint: {endpoint}')
         response = requests.get(endpoint)
+        response.raise_for_status()
+
         return response.json()
 
 
-print(MercadoBitcoinApi(coin='BTC').get_data())
+class DaySummaryApi(MercadoBitcoinApi):
+    type = 'day-summary'
+
+    def _get_endpoint(self, date: datetime.date):
+        return f'{self.base_endpoint}/{self.coin}/{self.type}/{date.year}/{date.month}/{date.day}'
+
+
+# print(DaySummaryApi(coin='BTC').get_data(date=datetime.date(2022, 11, 1)))
+
+
+class TradesAPI(MercadoBitcoinApi):
+    type = 'trades'
+
+    def get_unix_epoch(self, date: datetime.datetime):
+        return int(date.timestamp())
+
+    def _get_endpoint(self, date_from: datetime.datetime = None, date_to: datetime.datetime = None):
+        if date_from and not date_to:
+            unix_date_from = self.get_unix_epoch(date_from)
+            endpoint = f'{self.base_endpoint}/{self.coin}/{self.type}/{unix_date_from}'
+        elif date_from and date_to:
+            unix_date_from = self.get_unix_epoch(date_from)
+            unix_date_to = self.get_unix_epoch(date_to)
+            endpoint = f'{self.base_endpoint}/{self.coin}/{self.type}/{unix_date_from}/{unix_date_to}'
+        else:
+            endpoint = f'{self.base_endpoint}/{self.coin}/{self.type}'
+
+        return endpoint
+
+
+print(TradesAPI(coin='BTC').get_data())
+print(TradesAPI(coin='BTC').get_data(date_from=datetime.datetime(2022, 11, 1)))
+print(TradesAPI(coin='BTC').get_data(date_from=datetime.datetime(2022, 10, 1), date_to=datetime.datetime(2022, 10, 10)))
